acf8c558b585e6405559f0d3e6d797cc8212b7bb
choosungho <csh9895@naver.com>
2019-01-27 19:35:55 +0900

    send_sms

--- send_sms.js
+++ send_sms.js
@@ -1,18 +1,17 @@
 let request = require('request');
 
-module.exports = function(token, tel, data) {
+module.exports = function(option) {
     let form_data = {
         'Content' : {
-            'phone_number' : tel,
-            'message' : data,
-            'device_id' : "105918"
+            'phone_number' : option.phone_number,
+            'message' : option.message,
+            'device_id' : option.device_id
         }
     }
     let options = {
         url : 'https://smsgateway.me/api/v4/message/send',
         headers : {
-            'User-Agent' : 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
-            'Authorization' : token // token
+            'Authorization' : option.token // API token
         },
         body : JSON.stringify(form_data)
     }
